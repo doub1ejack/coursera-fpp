740c097a97e4e2090def5c79b8e573b4e3fa0c11
Micah <micah.mutrux@eatingwell.com>
2016-06-11 09:52:57 -0400

    pascal passes tests

--- wk1/src/main/scala/recfun/Main.scala
+++ wk1/src/main/scala/recfun/Main.scala
@@ -14,10 +14,21 @@ object Main {
    * Exercise 1
    */
     def pascal(c: Int, r: Int): Int = {
-      if(c-1 <= 0 || r-1 <= 0) {
-        1
-      } else {
-        0
+      (c,r) match {
+        case (c,r) if (c > r) => {
+          println(s"[$c,$r]=0")
+          0
+        }
+        case (c,r) if (r<=0 || c<=0) => {
+          println(s"($c,$r)=1")
+          1
+        }
+        case _ => {
+          val left = pascal(c-1, r-1)
+          val right = pascal(c, r-1)
+          println( s"($c,$r)="+ (left + right))
+          left + right
+        }
       }
     }
   
