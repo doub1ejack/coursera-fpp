dde0fcf11ec8946399d1ecaac3f1f623701c877f
Micah <micah.mutrux@eatingwell.com>
2016-06-11 08:33:05 -0400

    git init

---
+++ wk1/src/main/scala/recfun/Main.scala
@@ -0,0 +1,33 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      if(c-1 <= 0 || r-1 <= 0) {
+        1
+      } else {
+        0
+      }
+    }
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
